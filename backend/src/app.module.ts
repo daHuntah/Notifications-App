121b5c6780de2dd72e671cd031e26e26fb0da737
daHuntah <namhoang114.work@gmail.com>
2023-08-24 17:00:25 +0700

    Revert "Revert "Gọi API cho đăng kí""
    This reverts commit b5f76ed38996a6c3d7e4a345eebecae7a0553151.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/notification-app') ,
+    AuthModule,
+  ],
 })
 export class AppModule {}
+
+
+
+
