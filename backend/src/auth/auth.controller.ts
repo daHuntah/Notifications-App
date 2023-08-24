55f00fd17033ebc3fab788eadf23199e937913b1
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-08-24 14:19:21 +0700

    api register

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  
+  @Post('register')
+  async register(@Body() body: { username: string; password: string; email: string; phoneNumber: string }) {
+    const { username, password, email, phoneNumber } = body;
+    const result = await this.authService.register(username, password, email, phoneNumber);
+  
+    return {
+      statusCode: result.status,
+      message: result.message,
+     
+    };
+  }
+}
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from './schemas/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
