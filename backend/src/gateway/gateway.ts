1f8ef66a614014fc773e4fa87ae8c112346be178
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-08-31 03:07:20 +0700

    connect websocket

--- backend/src/gateway/gateway.ts
+++ backend/src/gateway/gateway.ts
@@ -3,11 +3,25 @@ import { OnModuleInit } from '@nestjs/common';
 import { Server } from "socket.io"
 
 @WebSocketGateway()
-export class MyGateway  {
+export class MyGateway implements OnModuleInit {
 
-  
+    @WebSocketServer()
+    server: Server
+
+    onModuleInit() {
+        this.server.on('connection', (socket) => {
+            console.log(socket.id);
+            console.log("Connected");
+        });
+    }
+
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
         console.log(body);
+        this.server.emit('onMessage',{
+            msg: "New msg",
+            content: body,
+        });
+        console.log('Data processed and event emitted.');
     }
 }
