620392d7e4fceb2690a69c1532c5674887acb442
tuanhieptran23 <trantuanhiep.work@gmail.com>
2023-08-31 14:31:50 +0700

    save data notification

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationService } from './notification.service';
 import { Notification, NotificationSchema } from './notification.model';
-
+import { NotificationController } from './notification.controller';
 @Module({
   imports: [MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }])],
   providers: [NotificationService],
+  controllers: [NotificationController],
   exports: [NotificationService],
 })
 export class NotificationModule {}
